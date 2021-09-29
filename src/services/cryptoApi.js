297ab0b07d865a67fec504ff9e4ee02a138b1ab8
Carlstong <carlstongrefalde@gmail.com>
2021-09-30 00:55:30 +0800

    Initial Crypto Tool

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,45 @@
+import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '987acac1aemsheda2af2aadea4bdp107051jsnb244a477f65f'
+}
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({
+    url, headers: cryptoApiHeaders
+})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl
+    }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`)
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
+        }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+        })
+    })
+})
+export const {
+    useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetExchangesQuery
+} = cryptoApi;
+
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/stats',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': '987acac1aemsheda2af2aadea4bdp107051jsnb244a477f65f'
+//     }
+//   };
